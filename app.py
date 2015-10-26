fecdd6588c38c2b200267943dcb2e0d42c21b786
Dave Sullivan <dave@dave-sullivan.com>
2015-10-25 21:28:34 -0400

    added state get/set method retrieval

--- app.py
+++ app.py
@@ -6,31 +6,39 @@ env = Environment()
 bridge = None
 
 def target_and_method_for(target_name):
+    env.start()
+    env.discover()
+    bridge = env.get_bridge('WeMo Link')
     groups = bridge.bridge_get_groups()
+    target, method = None, None
     if target_name in groups.keys():
         target = groups[target_name]
-        method = bridge.group_set_state
+        state_get = bridge.group_get_state
+        state_set = bridge.group_set_state
     else:
         lights = bridge.bridge_get_lights()
         if target_name in lights.keys():
             target = lights[target_name]
-            method = bridge.light_set_state
+            state_get = bridge.light_get_state
+            state_set = bridge.light_set_state
 
-    return (target, method)
+    return (target, state_get, state_set)
 
 def turn_lights_on(room=None, light=None):
     target_name = room or light
-    (target, method) = target_and_method_for(target_name)
+    print target_name
+    (target, state_get, state_set) = target_and_method_for(target_name[0])
+
     method(target, state=1)
 
 def turn_lights_off(room=None, light=None):
     target_name = room or light
-    (target, method) = target_and_method_for(target_name)
+    (target, state_get, state_set) = target_and_method_for(target_name[0])
     method(target, state=0)
 
 def dim_lights(level, room=None, light=None):
     target_name = room or light
-    (target, method) = target_and_method_for(target_name)
+    (target, state_get, state_set) = target_and_method_for(target_name[0])
     method(target, dim=int(level))
     
 @app.route('/lights/<action>')
@@ -46,12 +54,11 @@ def light_control(action):
 
     if method is not None:
         method(**request.args)
+        return "OK"
     else:
         return "Invalid action."
 
 
 if __name__ == '__main__':
-    env.start()
-    env.discover()
-    bridge = env.get_bridge('WeMo Link')
-    app.run(debug=True)
+    app.debug = True
+    app.run()
